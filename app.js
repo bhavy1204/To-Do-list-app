b626f75a744287475c2a51ddbfeaf0543b1a90de
bhavy1204 <bhavysarwa51@gmail.com>
2025-01-14 23:35:54 +0530

    Added task counter

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const inputElement = document.getElementById('input');
 const addTo = document.querySelector(".list")
 
+let count= document.querySelector("#taskCount");
+let cnt=0;
+
 document.addEventListener("DOMContentLoaded", load);
 
 function save(){
@@ -20,6 +23,8 @@ inputElement.addEventListener("keydown",(e)=> {
   if (e.key === "Enter") {
     let taskText = inputElement.value.trim();
     if (taskText != "") {
+        cnt++;
+        count.textContent=cnt;
         const NewTask = document.createElement('li');
         NewTask.textContent= taskText;
         // NewTask.classList.add('');
@@ -31,3 +36,5 @@ inputElement.addEventListener("keydown",(e)=> {
     console.log("Enter key pressed!");
   }
 });
+
+
