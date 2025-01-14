787fb11261cb0fae33ddd8c900d6a8d3b98d4de2
bhavy1204 <bhavysarwa51@gmail.com>
2025-01-14 15:58:04 +0530

    Added the lists

--- app.js
+++ app.js
@@ -1,2 +1,33 @@
-let task = document.createElement(p);
+const inputElement = document.getElementById('input');
+const addTo = document.querySelector(".list")
 
+document.addEventListener("DOMContentLoaded", load);
+
+function save(){
+    const tasks= Array.from(addTo.children).map(task=>task.textContent);
+    localStorage.setItem('tasks',JSON.stringify(tasks));
+}
+function load(){
+    let tasks = JSON.parse(localStorage.getItem('tasks') || '[]');
+    tasks.forEach(taskText => {
+        const NewTask = document.createElement('li');
+        NewTask.textContent=taskText;
+        addTo.appendChild(NewTask);
+    });
+}
+
+inputElement.addEventListener("keydown",(e)=> {
+  if (e.key === "Enter") {
+    let taskText = inputElement.value.trim();
+    if (taskText != "") {
+        const NewTask = document.createElement('li');
+        NewTask.textContent= taskText;
+        // NewTask.classList.add('');
+        addTo.appendChild(NewTask);
+        inputElement.value="";
+        save();
+    }
+
+    console.log("Enter key pressed!");
+  }
+});
